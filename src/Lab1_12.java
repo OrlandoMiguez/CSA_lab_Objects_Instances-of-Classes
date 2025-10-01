a5c765f66c71797c1715521c36ba187ebbae768c
OrlandoMiguez <omiguez26296@columbushs.com>
2025-10-01 11:53:12 -0400

    Update Lab1_12.java

--- src/Lab1_12.java
+++ src/Lab1_12.java
@@ -1,4 +1,4 @@
-
+import java.awt.Point;
 public class Lab1_12 {
     public static void main(String[] args) {
         System.out.println("Task 1: " + q1());
@@ -10,12 +10,17 @@ public class Lab1_12 {
     // 1. Create Point (5,10) and return coordinates
     public static String q1() {
         // TODO
+    Point p1 = new Point (5, 10);
+    String result = "" + p1.getX() + ", " + p1.getY();
+
         return "";
     }
 
     // 2. Move a Point (0,0) to (4,7) and return coordinates
     public static String q2() {
         // TODO
+        Point p1Point = new Point(0,0);
+        p1Point.move (4,7);
         return "";
     }
 
